7c7ceee9fcacfa4212bc48da08002f07873cebde
PearlRowe <146419328+PearlRowe@users.noreply.github.com>
2023-10-06 17:02:00 +0300

    Update and rename Addition of two numbers to Addition of two numbers.py

--- Addition of two numbers.py
+++ Addition of two numbers.py
@@ -4,5 +4,5 @@ name = input("enter your name here")
 print("hello" + name)
 Num1 =int(input("enter first number"))
 Num2 = int(input("enter second number"))
-#Sum = Num1 + Num2
-print =(name,"the sum of",Num1,"and", Num2,"is", Num1+ Num2)
+Sum = Num1 + Num2
+print (name,"the sum of",Num1,"and", Num2,"is", Num1+ Num2)
